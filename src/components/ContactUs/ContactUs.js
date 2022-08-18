8b97cf0858a21b4ecbe5042580bba306d2de2a1e
abhyudaysoni <abhyudaysoni03@gmail.com>
2022-08-18 13:52:00 +0530

    contact us section completed with form validity conditions

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactUs.css";
 import Button from "../UI/Button/Button";
 
 export default function ContactUs() {
+  const [email, setEmail] = useState("");
+  const [formValidity, setFormValidity] = useState(true);
+  const checkValidity = (e) => {
+    if (e.target.value.length === 0) {
+      setFormValidity(true);
+      setEmail("");
+      return;
+    }
+    if (e.target.value.includes("@") && e.target.value.includes(".")) {
+      setFormValidity(true);
+      setEmail(e.target.value);
+    } else {
+      setFormValidity(false);
+      setEmail(e.target.value);
+    }
+  };
+  const submitForm = () => {
+    console.log("form submitted");
+    setEmail("");
+    setFormValidity(true);
+  };
   return (
     <section className="contact-us">
       <h4>35,000+ already joined</h4>
       <h2>Stay up-to-date with what we're doing</h2>
       <div className="contact-field">
-        <input type="email" />
-        <Button className={"contact-us-btn"} name={"Contact Us"} />
+        <input
+          className={formValidity ? "valid" : "invalid"}
+          type="email"
+          value={email}
+          onChange={checkValidity}
+        />
+        <Button
+          className={"contact-us-btn"}
+          onClick={submitForm}
+          name={"Contact Us"}
+        />
       </div>
     </section>
   );
